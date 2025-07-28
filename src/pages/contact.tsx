8c0a9d592af00840805c15a82b8ee12abf261eb4
tm21cy <tm21cy@brocku.ca>
2025-07-27 22:20:00 -0400

    add stand-in for double blind reviews

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,9 +3,12 @@ import { Box, Typography } from '@mui/material';
 export default function Contact() {
   return (
     <Box sx={{ textAlign: 'center', py: 8 }}>
-      <Typography variant="h1" component="h1">
-        Coming soon.
+      <Typography variant="h1" component="h1" sx={{ mb: 2 }}>
+          Author Contacts
       </Typography>
+      <Typography variant="h5" component="h1" sx={{ mb: 2 }}>
+          <em>Due to the double-blind review process, we are unable to provide contact information for the authors at this time.</em>
+        </Typography>
     </Box>
   );
 } 
